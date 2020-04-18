7e45cc9ca33ecc6304b28377505b6c0268d1ed71
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-04-18 16:25:56 +0100

    Minor refactoring

--- ExpenseReportVerifier/Program.cs
+++ ExpenseReportVerifier/Program.cs
@@ -15,11 +15,11 @@ namespace Andy.ExpenseReport
             var statementFile = new FileInfo(args[1]);
             var expenseReportFile = new FileInfo(args[2]);
 
-            var parser = new RowParser();
-            var csvReader = new CsvFileReader();
+            var csvRowParser = new RowParser();
+            var csvFileReader = new CsvFileReader();
 
-            string[][] statementRows = ReadCsv(csvReader, parser, statementFile, delimiter);
-            string[][] transactionRows = ReadCsv(csvReader, parser, expenseReportFile, delimiter);
+            string[][] statementRows = ReadCsvFile(csvFileReader, csvRowParser, statementFile, delimiter);
+            string[][] transactionRows = ReadCsvFile(csvFileReader, csvRowParser, expenseReportFile, delimiter);
 
             var transactions = transactionRows.Select(TransactionDetailsParser.Parse);
             var statement = statementRows.Select(StatementEntryParser.Parse);
@@ -32,7 +32,7 @@ namespace Andy.ExpenseReport
             var results = matcher.Compare(statement, transactions.ToArray());
         }
 
-        private static string[][] ReadCsv(CsvFileReader csvReader, RowParser parser, FileInfo file, char delimiter)
+        private static string[][] ReadCsvFile(CsvFileReader csvReader, RowParser parser, FileInfo file, char delimiter)
         {
             return csvReader.Read(file, line => parser.Parse(line, delimiter));
         }
