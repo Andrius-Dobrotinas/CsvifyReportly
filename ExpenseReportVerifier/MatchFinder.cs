4b74f1af5245220d068c8260b123e912cc9f920e
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-04-18 13:05:11 +0100

    Moved a static function to the class that uses it because it modifies the state of the parameter, and there are less problems with that kind of thing when it's not "publicly" available.

--- ExpenseReportVerifier/MatchFinder.cs
+++ ExpenseReportVerifier/MatchFinder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Andy.ExpenseReport
 {
@@ -29,16 +28,34 @@ namespace Andy.ExpenseReport
 
             foreach (var statementEntry in statement)
             {
-                var transaction = MatchingTransactionFinder.GetFirstMatchingTransaction(
+                var transaction = GetFirstMatchingTransaction(
                     statementEntry,
-                    transactions,
-                    comparer);
+                    transactions);
 
                 if (transaction != null)
                     matches.Add(new Tuple<StatementEntry, TransactionDetails>(statementEntry, transaction));                
             }
 
             return matches;
+        }
+
+        private TransactionDetails GetFirstMatchingTransaction(
+            StatementEntry statementEntry,
+            IList<TransactionDetails> transactions)
+        {
+            for (int i = 0; i < transactions.Count; i++)
+            {
+                var transactionDetails = transactions[i];
+                if (transactionDetails == null) continue;
+
+                if (comparer.AreEqual(transactionDetails, statementEntry))
+                {
+                    transactions[i] = null;
+                    return transactionDetails;
+                }
+            }
+
+            return null;
         }
     }
 }
