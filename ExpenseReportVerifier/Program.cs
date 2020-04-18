612a37e2b338183686f39c058a04ae80ebc9e7e5
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-04-18 17:52:20 +0100

    Moved a few more things out of the main function

--- ExpenseReportVerifier/Program.cs
+++ ExpenseReportVerifier/Program.cs
@@ -12,11 +12,35 @@ namespace Andy.ExpenseReport
         static void Main(string[] args)
         {
             var statementFile = new FileInfo(args[1]);
-            var expenseReportFile = new FileInfo(args[2]);
+            var transactionsFile = new FileInfo(args[2]);
             var reportFile = new FileInfo(args[3]);
+            
+            var sourceData = ReadSourceData(statementFile, transactionsFile, delimiter);
 
-            string[][] statementRows = ReadCsvFile(statementFile, delimiter);
-            string[][] transactionRows = ReadCsvFile(expenseReportFile, delimiter);
+            var matcher = new CollectionComparer(
+                new MatchFinder(
+                    new ItemComparer(
+                        new MerchantComparer())));
+
+            var result = matcher.Compare(sourceData.StatementEntries, sourceData.Transactions.ToArray());
+
+            string[] lines = StringyfyyResults(
+                result,
+                sourceData.StatementColumnCount,
+                sourceData.TransactionColumnCount);
+
+            Csv.IO.CsvFileWriter.Write(lines, reportFile);
+
+            Console.WriteLine("Done");
+        }
+
+        private static SourceData ReadSourceData(
+            FileInfo statementFile,
+            FileInfo transactionsFile,
+            char csvDelimiter)
+        {
+            string[][] statementRows = ReadCsvFile(statementFile, csvDelimiter);
+            string[][] transactionRows = ReadCsvFile(transactionsFile, csvDelimiter);
 
             if (!statementRows.Any()) throw new Exception("Statement file is empty");
             if (!transactionRows.Any()) throw new Exception("Transactions file is empty");
@@ -31,20 +55,15 @@ namespace Andy.ExpenseReport
                 throw new Exception("All statement rows must have the same number of columns");
 
             var transactions = transactionRows.Select(TransactionDetailsParser.Parse);
-            var statement = statementRows.Select(StatementEntryParser.Parse);
+            var statementEntries = statementRows.Select(StatementEntryParser.Parse);
 
-            var matcher = new CollectionComparer(
-                new MatchFinder(
-                    new ItemComparer(
-                        new MerchantComparer())));
-
-            var result = matcher.Compare(statement, transactions.ToArray());
-
-            string[] lines = StringyfyyResults(result, statementColumnCount, transactionColumnCount);
-
-            Csv.IO.CsvFileWriter.Write(lines, reportFile);
-
-            Console.WriteLine("Done");
+            return new SourceData
+            {
+                Transactions = transactions.ToArray(),
+                TransactionColumnCount = transactionColumnCount,
+                StatementEntries = statementEntries.ToArray(),
+                StatementColumnCount = statementColumnCount
+            };
         }
 
         private static string[][] ReadCsvFile(FileInfo file, char delimiter)
---
+++ ExpenseReportVerifier/SourceData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Andy.ExpenseReport
+{
+    public class SourceData
+    {
+        public IList<TransactionDetailsWithSourceData> Transactions { get; set; }
+        public IList<StatementEntryWithSourceData> StatementEntries { get; set; }
+        public int TransactionColumnCount { get; set; }
+        public int StatementColumnCount { get; set; }
+    }
+}
