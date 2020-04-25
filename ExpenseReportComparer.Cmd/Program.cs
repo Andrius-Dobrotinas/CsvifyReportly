ffdb690ccc460dbaa7ab22ce83f226a59c585b12
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-04-25 12:00:42 +0100

    Moved object graph construction outside the main method

--- ExpenseReportComparer.Cmd/Program.cs
+++ ExpenseReportComparer.Cmd/Program.cs
@@ -45,34 +45,8 @@ namespace Andy.ExpenseReport.Verifier.Cmd
 
             try
             {
-                var item1Parser = new Comparison.Csv.Bank.StatementEntryParser(settings.StatementCsvFile.ColumnIndexes);
-                var item2Parser = new Comparison.Csv.Bank.TransactionDetailsParser(settings.TransactionsCsvFile.ColumnIndexes);  
-
-                var collectionComparer = new Comparison.CollectionComparer<
-                    Comparison.Csv.Bank.StatementEntryWithSourceData,
-                    Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
-                new Comparison.Statement.Bank.MatchFinder<
-                    Comparison.Csv.Bank.StatementEntryWithSourceData,
-                    Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
-                    new Comparison.Statement.Bank.ItemComparer(
-                        new Comparison.Statement.Bank.MerchantNameComparer(
-                            new Comparison.Statement.Bank.MerchanNameVariationComparer(
-                                settings.MerchantNameMap)))));
-
-                var comparer = new Comparison.Csv.Comparer<
-                    Comparison.Csv.Bank.StatementEntryWithSourceData,
-                    Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
-                        collectionComparer,
-                        item1Parser,
-                        item2Parser);
-
-                var fileComparer = new ReportingFileComparer<
-                        Comparison.Csv.Bank.StatementEntryWithSourceData,
-                        Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
-                        new Comparison.Csv.CsvStream.ReportingComparer<
-                            Comparison.Csv.Bank.StatementEntryWithSourceData,
-                            Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
-                                comparer));
+                var comparer = BuildBankStatementComparer(settings);
+                var fileComparer = BuildFileComparer(comparer);
 
                 fileComparer.CompareAndWriteReport(
                     parameters.StatementFile,
@@ -84,7 +58,7 @@ namespace Andy.ExpenseReport.Verifier.Cmd
             {
                 Console.Error.WriteLine(e.Message);
                 Console.Error.WriteLine(e.ActualException?.Message);
-                
+
                 return ResolveReturnCode(e);
             }
             catch (Exception e)
@@ -120,6 +94,46 @@ namespace Andy.ExpenseReport.Verifier.Cmd
                 .ToDictionary(
                     x => x.Key,
                     x => x.Value);
+        }
+
+        private static Comparison.Csv.Comparer<
+                Comparison.Csv.Bank.StatementEntryWithSourceData,
+                Comparison.Csv.Bank.TransactionDetailsWithSourceData>
+            BuildBankStatementComparer(
+                Bank.ExpenseReportParameters<
+                    Comparison.Csv.Bank.StatementEntryColumnIndexes,
+                    Comparison.Csv.Bank.TransactionDetailsColumnIndexes> settings)
+        {
+            var item1Parser = new Comparison.Csv.Bank.StatementEntryParser(settings.StatementCsvFile.ColumnIndexes);
+            var item2Parser = new Comparison.Csv.Bank.TransactionDetailsParser(settings.TransactionsCsvFile.ColumnIndexes);
+
+            var collectionComparer = new Comparison.CollectionComparer<
+                Comparison.Csv.Bank.StatementEntryWithSourceData,
+                Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
+                new Comparison.Statement.Bank.MatchFinder<
+                    Comparison.Csv.Bank.StatementEntryWithSourceData,
+                    Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
+                    new Comparison.Statement.Bank.ItemComparer(
+                        new Comparison.Statement.Bank.MerchantNameComparer(
+                            new Comparison.Statement.Bank.MerchanNameVariationComparer(
+                                settings.MerchantNameMap)))));
+
+            var comparer = new Comparison.Csv.Comparer<
+                Comparison.Csv.Bank.StatementEntryWithSourceData,
+                Comparison.Csv.Bank.TransactionDetailsWithSourceData>(
+                    collectionComparer,
+                    item1Parser,
+                    item2Parser);
+
+            return comparer;
+        }
+
+        private static ReportingFileComparer<TItem1, TItem2> BuildFileComparer<TItem1, TItem2>(
+             Comparison.Csv.IComparer<TItem1, TItem2> comparer)
+        {
+            return new ReportingFileComparer<TItem1, TItem2>(
+                    new ReportingComparer<TItem1, TItem2>(
+                            comparer));
         }
     }
 }
