da4663f5793fae7cba31d4932829125453194705
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-07-12 12:29:28 +0100

    Field names improved

--- Collections/Collections/EnumerableCopyManager.cs
+++ Collections/Collections/EnumerableCopyManager.cs
@@ -9,8 +9,12 @@ namespace Andy.Collections
 
         private readonly IList<T> store;
 
-        private int poseesh = defaultPosition;
-        private int size = defaultPosition;
+        /* Both size and position are zero-based so they can be easily compared to each
+         * other without having to add or subtract a number each time.
+         * Keeping a track of size like that (instead of getting it from the store)
+         * for performance considerations */
+        private int currentPoseesh = defaultPosition;
+        private int sizeZeroBased = defaultPosition;
 
         public EnumerableCopyManager(IList<T> store)
         {
@@ -19,24 +23,25 @@ namespace Andy.Collections
             if (store.Count > 0) throw new ArgumentException("The storage collection must be empty");
         }
 
-        public T Current => store[poseesh];
-        public bool IsEndOfTheLine => poseesh == size;
+        public T Current => store[currentPoseesh];
+        public bool IsEndOfTheLine => currentPoseesh == sizeZeroBased;
 
         public void AdvancePosition()
         {
-            poseesh++;
+            currentPoseesh++;
         }
 
         public void Reset()
         {
-            poseesh = defaultPosition;
+            currentPoseesh = defaultPosition;
         }
 
         public void Add(T value)
         {
-            poseesh++;
-            size++;
+            currentPoseesh++;
+            sizeZeroBased++;
             store.Add(value);
         }
     }
 }
+
