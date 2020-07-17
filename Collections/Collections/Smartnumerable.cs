d61b991d5f3d6a34b215514d5f1941ba29f4cac6
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-07-17 17:58:17 +0100

    [SMARTNUMERABLE] An interface for Smartnumerable

--- Collections/Collections/Smartnumerable.cs
+++ Collections/Collections/Smartnumerable.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 
 namespace Andy.Collections
 {
-    /// <summary>
-    /// An enumerable that uses <see cref="Smartnumerator{T}"/> type of enumerator 
-    /// that allows to enumerate enumerables that are expensive to enumerate
-    /// (or only enumerate once) multiple times without the original cost on
-    /// subsequent enumerations.
-    /// Refer to the documentation of <see cref="Smartnumerator{T}"/> for more details
-    /// </summary>
-    public class Smartnumerable<TValue> : IEnumerable<TValue>
+    /// <inheritdoc/>
+    public class Smartnumerable<TValue> : ISmartnumerable<TValue>
     {
         private readonly Smartnumerator<TValue> enumerator;
 
