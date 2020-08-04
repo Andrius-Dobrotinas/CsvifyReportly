3166b834e65b75d851c66293ab00623c66a84a6c
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-08-04 21:49:51 +0100

    [DIAGNOSTICS] Rudimentary reporting of the progress and results of transformations

--- Csv.Transformation/Row/Document/DocumentTransformer.cs
+++ Csv.Transformation/Row/Document/DocumentTransformer.cs
@@ -30,7 +30,38 @@ namespace Andy.Csv.Transformation.Row.Document
 
             var actualTransformer = factory.Build(columnIndexes);
 
-            return transformerRunner.Transform(document, actualTransformer);
+            /* todo: maybe i should add a Name property to the transformer and have
+             * that set by each factory? because the type name would be the same in
+             * many cases. that would help identify specific configurations (each
+             * config could have a name, by the way) */
+            Console.WriteLine($"Running transformer {actualTransformer.GetType()}");
+
+            var result = transformerRunner.Transform(document, actualTransformer);
+
+            Console.WriteLine("Transformer finished");
+
+            if (result.ContentRows.Length != document.ContentRows.Length)
+            {
+                Console.WriteLine("The following rows have been filtered out:");
+
+                foreach (var row in document.ContentRows.Except(result.ContentRows))
+                    Console.WriteLine(Stringicize(row));
+            }
+
+            if (result.HeaderCells.Length != document.HeaderCells.Length)
+            {
+                Console.WriteLine("Columns have been added/removed. The document is now made up of these columns:");
+                Console.WriteLine(Stringicize(result.HeaderCells));
+
+                // todo: even if the length hasn't change, check whether the names (or positions) have.
+            }
+
+            return result;
+        }
+
+        private string Stringicize(string[] row)
+        {
+            return string.Join('|', row);
         }
     }
 }
