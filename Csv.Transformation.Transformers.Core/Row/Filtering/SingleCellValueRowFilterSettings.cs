82e085b1c358553b0a0ad80e838393ad0f90c650
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-08-09 10:25:54 +0100

    Single Cell Value Row Filter for PayPal transaction filtering added

---
+++ Csv.Transformation.Transformers.Core/Row/Filtering/SingleCellValueRowFilterSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Andy.Csv.Transformation.Row.Filtering
+{
+    public class SingleCellValueRowFilterSettings : TransformerSettings
+    {
+        public string TargetColumnName { get; set; }
+        public string TargetValue { get; set; }
+
+        public override IDocumentTransformerFactory BuildFactory()
+        {
+            var name = this.GetDescription();
+
+            return new SingleCellValueEvaluatorFactory(
+                name,
+                TargetColumnName,
+                    new StraightforwardValueComparer(TargetValue));
+        }
+    }
+}
