c9480130c5f3511b84602fbfb14ee9e2b44cb1c4
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2021-02-12 21:08:23 +0000

    I need to know which column names are not unique

--- Csv/Util/ArrayValueUniquenessChecker.cs
+++ Csv/Util/ArrayValueUniquenessChecker.cs
@@ -6,17 +6,35 @@ namespace Andy.Csv
 {
     public interface IArrayValueUniquenessChecker
     {
-        bool HasDuplicates(IEnumerable<string> values);
+        bool HasDuplicates(IEnumerable<string> values, out string[] nonUniqueColumnNames);
     }
 
     public class ArrayValueUniquenessChecker : IArrayValueUniquenessChecker
     {
-        public bool HasDuplicates(IEnumerable<string> values)
+        public bool HasDuplicates(IEnumerable<string> values, out string[] nonUniqueColumnNames)
         {
-            if (values.Any() == false) return false;
+            if (values.Any() == false)
+            {
+                nonUniqueColumnNames = null;
+                return false;
+            }
 
             var uniqueValues = values.Distinct();
-            return values.Count() != uniqueValues.Count();
+            if (values.Count() == uniqueValues.Count())
+            {
+                nonUniqueColumnNames = null;
+                return false;
+            }
+            else
+            {
+                nonUniqueColumnNames = values
+                    .GroupBy(x => x)
+                    .Where(x => x.Count() > 1)
+                    .Select(x => x.Key)
+                    .ToArray();
+
+                return true;
+            }
         }
     }
 }
