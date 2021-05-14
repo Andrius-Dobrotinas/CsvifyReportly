9dfa8d24de70c5824248fb15867814b4b511dfe2
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2021-05-14 19:15:04 +0100

    Third-level transaction lookup where transaction details string is no longer at play -- at this point, it was unable to find matches based on the details string. So it should try to at least see if it can find matches based on amounts and dates.

---
+++ ExpenseReport.Comparison/Statement/AlwaysEqualComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Andy.ExpenseReport.Comparison.Statement
+{
+    public class AlwaysEqualComparer : IDetailsComparer
+    {
+        public bool AreEqual(string statementDetailsString, string name)
+        {
+            return true;
+        }
+    }
+}
---
+++ ExpenseReportComparer.Cmd/DetailsSkippedComparerSettings.cs
@@ -0,0 +1,26 @@
+﻿using Andy.ExpenseReport.Comparison;
+using Andy.ExpenseReport.Comparison.Csv.Statement;
+using System;
+using System.Collections.Generic;
+
+namespace Andy.ExpenseReport.Verifier.Cmd
+{
+    public class DetailsSkippedComparerSettings : ComparerSettings
+    {
+        public int DateTolerance { get; set; }
+        public ExpenseReport.Comparison.Statement.Bank.DateComparisonDirection Direction { get; set; }
+
+        public override IItemComparer<
+            StatementEntryWithSourceData,
+            StatementEntryWithSourceData> BuildComparer()
+        {
+            return 
+                new Comparison.Statement.ItemComparer(
+                    new Comparison.Statement.AlwaysEqualComparer(),
+                    new Comparison.Statement.Bank.InvertedAmountComparer(),
+                    new Comparison.Statement.Bank.TolerantDateComparer(
+                        this.DateTolerance,
+                        this.Direction));
+        }
+    }
+}
